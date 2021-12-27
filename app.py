b14d188a7b5c08f603a28f98ce2839aa1ed525b5
hoo <hoo0681@gachon.ac.kr>
2021-12-27 15:07:14 +0900

    학습 실패 처리

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ app = FastAPI()
 
 class FLclient_status(BaseModel):
     FLCLstart: bool = False
+    FLCFail:bool=False
 
 
 status = FLclient_status()
@@ -38,7 +39,7 @@ def build_model():
 
 
 # Define Flower client
-class CifarClient(fl.client.NumPyClient):
+class MnistClient(fl.client.NumPyClient):
     def __init__(self, model, x_train, y_train, x_test, y_test):
         self.model = model
         self.x_train, self.y_train = x_train, y_train
@@ -60,10 +61,6 @@ class CifarClient(fl.client.NumPyClient):
 
 model=build_model()
 # Load CIFAR-10 dataset
-mnist = tf.keras.datasets.mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
 
 
 @app.on_event("startup")
@@ -93,12 +90,17 @@ async def flower_client_start():
     print('learning')
     global status
     global model
-    global x_train
-    global x_test
-    global y_train
-    global y_test
-    fl.client.start_numpy_client(server_address="10.152.183.181:8080", client=CifarClient(model,x_train,y_train, x_test, y_test))
-    await model_save()
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    try:
+        fl.client.start_numpy_client(server_address="10.152.183.181:8080", client=MnistClient(model, x_train, y_train, x_test, y_test))
+        await model_save()
+    except Exception as e:
+        print('[E] learning',e)
+        status.FLCFail=True
+        await notify_fail()
+        status.FLCFail=False
 
 
 async def model_save():
@@ -122,7 +124,19 @@ async def notify_fin():
             print(r.content)
         await asyncio.sleep(5)
 
-
+async def notify_fail():
+    global status
+    status.FLCLstart = False
+    while True:
+        print('try')
+        r = requests.get('http://localhost:8080/trainFail')
+        print('try')
+        if r.status_code == 200:
+            print('trainFin')
+            break
+        else:
+            print(r.content)
+        await asyncio.sleep(5)
 # Start Flower client
 # s3에 model 없고 환경변수를 탐색하여 ENV가 init이라면 s3에 초기 가중치를 업로드 한다.
 from botocore.exceptions import ClientError
