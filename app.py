ea2dcac5c5d02ae3fa100ca851cc368310fe50a9
hoo <hoo0681@gachon.ac.kr>
2021-12-27 15:16:56 +0900

    set_memory_growth

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ import logging
 import json
 import boto3
 
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 app = FastAPI()
 
 
