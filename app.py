2b55b97399e0f64940df984e334bb631b5856a87
hoo <hoo0681@gachon.ac.kr>
2021-12-27 15:34:02 +0900

    GPU없어도 됨

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ import json
 import boto3
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 app = FastAPI()
 
